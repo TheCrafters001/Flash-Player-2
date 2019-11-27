1bf4a53383b2e012500e90a29e67c71c33f7f738
TheCrafters001 <46639269+TheCrafters001@users.noreply.github.com>
2019-11-27 10:55:30 -0500

    Upload issue code

--- src/Flash Player 2/Video/Media_Player.Designer.vb
+++ src/Flash Player 2/Video/Media_Player.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class Media_Player
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,30 +20,21 @@ Partial Class Media_Player
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Media_Player))
-        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
         Me.VlcControl1 = New Vlc.DotNet.Forms.VlcControl()
         CType(Me.VlcControl1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
-        'MenuStrip1
-        '
-        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
-        Me.MenuStrip1.Name = "MenuStrip1"
-        Me.MenuStrip1.Size = New System.Drawing.Size(800, 24)
-        Me.MenuStrip1.TabIndex = 0
-        Me.MenuStrip1.Text = "MenuStrip1"
-        '
         'VlcControl1
         '
         Me.VlcControl1.BackColor = System.Drawing.Color.Black
-        Me.VlcControl1.Location = New System.Drawing.Point(12, 27)
+        Me.VlcControl1.Location = New System.Drawing.Point(12, 35)
         Me.VlcControl1.Name = "VlcControl1"
-        Me.VlcControl1.Size = New System.Drawing.Size(776, 411)
+        Me.VlcControl1.Size = New System.Drawing.Size(776, 403)
         Me.VlcControl1.Spu = -1
-        Me.VlcControl1.TabIndex = 1
+        Me.VlcControl1.TabIndex = 0
         Me.VlcControl1.Text = "VlcControl1"
         Me.VlcControl1.VlcLibDirectory = CType(resources.GetObject("VlcControl1.VlcLibDirectory"), System.IO.DirectoryInfo)
         Me.VlcControl1.VlcMediaplayerOptions = Nothing
@@ -54,16 +45,12 @@ Partial Class Media_Player
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
         Me.Controls.Add(Me.VlcControl1)
-        Me.Controls.Add(Me.MenuStrip1)
-        Me.MainMenuStrip = Me.MenuStrip1
         Me.Name = "Media_Player"
         Me.Text = "Media_Player"
         CType(Me.VlcControl1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
-        Me.PerformLayout()
 
     End Sub
 
-    Friend WithEvents MenuStrip1 As MenuStrip
     Friend WithEvents VlcControl1 As Vlc.DotNet.Forms.VlcControl
 End Class
